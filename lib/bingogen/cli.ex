b65715e96dc825cdf3b4d48d1da8d57878668ec4
Robert Krauss <robert.krauss.de.@gmail.com>
2015-11-23 11:54:13 +0100

    Removed invalid constant definitions from CLI.

--- lib/bingogen/cli.ex
+++ lib/bingogen/cli.ex
@@ -1,10 +1,4 @@
 defmodule Bingogen.CLI do
-  @default_count 4
-  @module """
-  Handle the command line parsing and the dispatch to
-  the varios functions that end up generating a
-  table of the last _n_ issues in a github project
-  """
   def main(argv) do
     argv
     |> parse_args
